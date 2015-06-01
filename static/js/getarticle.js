7a88b144825a1a928ced223b36db32c67c88b168
RobinBonnin <robin.bonnin@insa-lyon.fr>
2015-06-02 00:11:31 +0200

    getarticle js

--- static/js/getarticle.js
+++ static/js/getarticle.js
@@ -1,5 +1,18 @@
 window.onload=function() {
-        $.get("/item", function(data, status){
-            alert("Data: " + data + "\nStatus: " + status);
-        })
+        $.ajax({
+        		url: '/item',
+        		type: 'GET',
+        		dataType: 'json',
+        		success: function(response) {
+                    console.log(response);
+                if(response == 'error') {
+                	document.getElementById('erreurarticle').innerHTML = "On verra";
+                }
+                else {
+                	console.log("Bien affiché  ")
+                	window.location.replace('/item');
+                    alert("Document bien affiché");
+                }
+            }
+        		}) 
 }
